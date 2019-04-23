a44f99731e25ee82ac4bc0bacbfdcd3c6ef6e03c
concurrent-input-6175 <xywong23@gmail.com>
2019-04-23 04:59:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,7 @@
 
 def valid_move?(board,index)
   if board = ['','','','','','','','',''] || board !=  ['','','','','','','','',''] 
+     index.beween?(0.8)
     true
   else index.between?(0,8)
     true
