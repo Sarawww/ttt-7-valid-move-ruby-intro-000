910d23023377bfa545bc9086640f90084230c3b4
concurrent-input-6175 <xywong23@gmail.com>
2019-04-23 05:12:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,23 +1,13 @@
 # code your #valid_move? method here
 
-def valid_move?(board,index)
-  if position_taken?
-    false
-  if board = ['','','','','','','','',''] || board !=  ['','','','','','','','',''] 
-    true
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
   end
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == ""
-    false
-    elsif board[index] == nil 
-    false
-    elsif index.between?(0.8)
-    false
-  else board[index] == "X" || board[index] == "O"
-    true
-  end
+  board[index] != " "
 end
